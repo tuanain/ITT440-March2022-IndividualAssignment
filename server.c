18723980cfa3840dd20a36c9e60944ed48ed2218
Tuan Ain <2020865508@student.uitm.edu.my>
2022-05-24 22:34:29 +0800

    stuck

--- server.c
+++ server.c
@@ -8,7 +8,32 @@
 
 int recive_image(int socket)
 {
-int size;
-char image_array[];
-FILE *fpimage;
+
+	int size;
+	char image_array[];
+	FILE *fpimage;
+
+	do
+	{
+		status= read(socket, &size, sizeof(int));
+	}
+	while(status<0)
+	{
+		printf("RECEIVED :\nPacket received");
+
+		char buffer[]="success!";
+
+	}
+	do 
+	{
+		status=write(socket, &buffer);
+	}	while(status<0);
+		printf("Reply is successful\n");
+		print(" \n");
+	if(fpimage==null)
+	{
+		printf("error occured");
+		return;
+	}
+
 }
