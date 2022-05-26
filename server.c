0a8dc656c7a8f3e7def5da7fcb9041f3771be5c4
Tuan Ain <2020865508@student.uitm.edu.my>
2022-05-26 13:23:11 +0800

    main fx

--- server.c
+++ server.c
@@ -37,3 +37,27 @@ int recive_image(int socket)
 	}
 
 }
+
+	int parseARGS(char **args, char *line)
+	{
+		int tmp = 0;
+		args[tmp] = strtok(line, ":");
+		while ((args[++tmp] = strtok)NULL, ":")) != NULL);
+		return tmp -1;
+	}
+	int count1=1, count2=1, percent;
+	char *header[4096];
+	int cont=0, create_socket, new_socket, addrlen, fd;
+
+	int main(int argc, char **argv)
+	{
+		char fname[256];
+		int bufsize=1024,n;
+		buffer=malloc(bufsize);
+		struct sockaddr_in address;
+	if ((create_socket=socket (AF_INET, SOCK_STREAM, 0));
+	printf ("the socket was created");
+		address.sin_family=AF_INET;
+		address.sin_addr.s_addr=INADDR_ANY;
+		address.sin_port=htons(7000);
+	}
