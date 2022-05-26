ec16d2c14581f533c5ce00e769e30e0c386d952b
Tuan Ain <2020865508@student.uitm.edu.my>
2022-05-26 13:21:17 +0800

    declare

--- client.c
+++ client.c
@@ -3,3 +3,26 @@
 	Protocol: SMTP
 
 */
+
+ #include<string.h>
+ #include<sys/socket.h>
+ 
+ int main(int argc, char *argv[])
+ {
+	int create_socket;
+	int bufsize=1024;
+	int count=0;
+	char *buffer=malloc(bufsize);
+	char fname[256], passk[10], logink[10];
+	struct log;
+	char login[];
+	char pass[];
+	log[];
+	int opt,i=0;
+	struct sockaddre_in address;
+	File *file_to_sent;
+	int ch;
+	char toSEND[];
+	char remoteFile[4096];
+	int count1=1, count 2=1, percent;
+ }
