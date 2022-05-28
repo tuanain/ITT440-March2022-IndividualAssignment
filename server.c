fd1db6733d39e5723182c23e604209bc52c6ae1a
Tuan Ain <2020865508@student.uitm.edu.my>
2022-05-29 07:51:50 +0800

    update server

--- server.c
+++ server.c
@@ -12,52 +12,74 @@ int recive_image(int socket)
 	int size;
 	char image_array[];
 	FILE *fpimage;
+	int recvsize;
+    int size;
+    int readsize;
+    int writesize;
+    int packetindex;
+    int status;
 
 	do
 	{
 		status= read(socket, &size, sizeof(int));
 	}
 	while(status<0)
-	{
+
 		printf("RECEIVED :\nPacket received");
-
+		printf(" \n");
 		char buffer[]="success!";
-
-	}
-	do 
+	do
 	{
 		status=write(socket, &buffer);
-	}	while(status<0);
+	}
+	while(status<0);
+
 		printf("Reply is successful\n");
 		print(" \n");
+
+	fpimage=fopen("received.jpeg", "w");
+
 	if(fpimage==null)
 	{
 		printf("error occured");
-		return;
+		return -1;
 	}
 
+	struct timeval timeout = {10,0};
+
+    	fd_set fd_s;
+    	int bufferfd;
+
+    	while(recvsize < size) {
+        FD_ZERO(&fd_s);
+        FD_SET(socket,&fd_s);
+
+        bufferfd = select(FD_SETSIZE,&fd_s,NULL,NULL,&timeout);
+
+        if (bufferfd < 0)
+            printf("Error occured due to bad file descriptor set.\n");
+
+        if (bufferfd == 0)
+            printf("Error occured due to buffer read timeout expired.\n");
+        if(bufferfd > 0)
+        {
+            do{
+                readsize = read(socket, img_array, 10240);
+            }while(readsize < 0);
+
+            writesize = fwrite(img_array,1,readsize, fpimage);
+            printf("Image size written : %i\n",writesize);
+
+            if(readsize!=writesize)
+            {
+                printf("Error occured while reading!\n");
+            }
+
+            recvsize += readsize;
+            packetindex++;
+            printf("Total Received Image size : %i\n",recvsize);
+            printf(" \n");
+	}
+    }
 }
 
-	int parseARGS(char **args, char *line)
-	{
-		int tmp = 0;
-		args[tmp] = strtok(line, ":");
-		while ((args[++tmp] = strtok)NULL, ":")) != NULL);
-		return tmp -1;
-	}
-	int count1=1, count2=1, percent;
-	char *header[4096];
-	int cont=0, create_socket, new_socket, addrlen, fd;
-
-	int main(int argc, char **argv)
-	{
-		char fname[256];
-		int bufsize=1024,n;
-		buffer=malloc(bufsize);
-		struct sockaddr_in address;
-	if ((create_socket=socket (AF_INET, SOCK_STREAM, 0));
-	printf ("the socket was created");
-		address.sin_family=AF_INET;
-		address.sin_addr.s_addr=INADDR_ANY;
-		address.sin_port=htons(7000);
-	}
