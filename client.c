8cf06e8e97760d9b67f8bf03fb1848f34f20a2f1
Tuan Ain <2020865508@student.uitm.edu.my>
2022-05-29 16:34:29 +0800

    update

--- client.c
+++ client.c
@@ -148,7 +148,7 @@ void send_image(int socket)
     	do
     	{
         	printf("Enter the command : ");
-        	gets(cname);  
+        	intake: fgets(cname);  
         	//cname[strlen(cname) + 1] = '\0';
         	char code[4];//to store the 3 digit response code received from server
 
